0282bea19aabb5b786ea933107543de8c2c926d0
Michael Quinnemail <mquinn5@binghamton.edu>
2018-03-18 16:46:07 -0400

    created and using BST Nodes for history for bimodal predictors

--- Documents/CS 320/cs320_project1/Predictors.cpp
+++ Documents/CS 320/cs320_project1/Predictors.cpp
@@ -3,6 +3,58 @@
 
 using namespace std;
 
+Node<T>::Node(unsigned long address, T data){
+	this->address = address;
+	this->data = data;
+}
+
+bool Node<T>::add(unsigned long address, T data){
+	if(address<this->address){
+		if(this->left==NULL)
+			this->left = new Node(address, data);
+		else
+			return this->left->add(address, data);
+	} else if(address>this->address){
+		if(this->right==NULL)
+			this->right = new Node(address, data);
+		else
+			return this->right->add(address, data);
+	}
+	return false;
+}
+
+bool Node<T>::has(unsigned long address){
+	if(this->address==address)
+		return true;
+	if(address<this->address && this-left!=NULL)
+		return this->left->has(address);
+	if(address>this->address && this->right!=NULL)
+		return this->right->has(address);
+	return false;
+}
+
+T Node<T>::get(unsigned long address){
+	if(this->address=address)
+		return this->data;
+	if(address<this->address && this-left!=NULL)
+		return this->left->get(address);
+	if(address>this->address && this->right!=NULL)
+		return this->right->get(address);
+	return T;
+}
+
+bool Node<T>::set(unsigned long address, T data){
+	if(this->address=address){
+		this->data=data;
+		return true;
+	}
+	if(address<this->address && this-left!=NULL)
+		return this->left->set(address, data);
+	if(address>this->address && this->right!=NULL)
+		return this->right->set(address, data);
+	return false;
+}
+
 double AlwaysTaken::predict(ifstream * file){
 	string str;
 	while(getline(*file, str)){
@@ -27,17 +79,18 @@ double SingleBimodal::predict(ifstream * file){
 	string str;
 	while(getline(*file, str)){
 		this->total++;
-		unsigned long index = stoi(str.substr(2,8), 0, 16);
-		cout << "test1" << endl;
-		bool num = this->history[index];
-		cout << "test2" << endl;
-		if(str[11]=='T' && num)
+		unsigned long address = stoi(str.substr(2,8), 0, 16);
+		if(this->history==NULL)
+			this->history=new Node<bool>(address, false);
+		if(!this->history->has(address))
+			this->history->add(address, false);
+		bool data = this->history->get(address);
+		if(str[11]=='T' && data)
 			this->correct++;
-		else if(str[11]=='N' && !num)
+		else if(str[11]=='N' && !data)
 			this->correct++;
 		else
-			this->history[index]=!num;
-		cout << "test3" << endl;
+			this->history->set(address, !data);
 	}
 	return this->percent();
 }
@@ -46,19 +99,23 @@ double DoubleBimodal::predict(ifstream * file){
 	string str;
 	while(getline(*file, str)){
 		this->total++;
-		unsigned long index = stoi(str.substr(2,8), 0, 16);
-		int num = this->history[index];
+		unsigned long address = stoi(str.substr(2,8), 0, 16);
+		if(this->history==NULL)
+			this->history=new Node<int>(address, 0);
+		if(!this->history->has(address))
+			this->history->add(address, 0);
+		int data = this->history->get(address);
 		if(str[11]=='T'){
-			if(num>1)
+			if(data>1)
 				this->correct++;
-			if(num<3)
-				this->history[index]++;
+			if(data<3)
+				this->history->set(address, data+1);
 		}
 		else if(str[11]=='N'){
-			if(num>1)
+			if(data>1)
 				this->correct++;
-			if(num>0)
-				this->history[index]--;
+			if(data>0)
+				this->history->set(address, data-1);
 		}
 	}
 	return this->percent();
--- Documents/CS 320/cs320_project1/Predictors.h
+++ Documents/CS 320/cs320_project1/Predictors.h
@@ -6,6 +6,22 @@
 #include <vector>
 #include <string>
 
+template <class T>
+class Node{
+	private:
+		Node * left;
+		Node * right;
+		unsigned long address;
+		T data;
+	public:
+		Node(unsigned long, T);
+		~Node(){delete left; delete right; left=right=NULL;};
+		bool add(unsigned long, T);
+		bool has(unsigned long);
+		T get(unsigned long);
+		bool set(unsigned long, T);
+};
+
 class AlwaysTaken{
 	private:
 		long correct;
@@ -28,22 +44,22 @@ class NeverTaken{
 
 class SingleBimodal{
 	private:
-		std::vector<bool> history;
+		Node<bool>* history;
 		long correct;
 		long total;
 	public:
-		SingleBimodal(){this->correct=this->total=0;};
+		SingleBimodal(){this->correct=this->total=0;this->history=NULL;};
 		double percent(){return ((double) this->correct)/this->total;};
 		double predict(std::ifstream*);
 };
 
 class DoubleBimodal{
 	private:
-		std::vector<int> history;
+		Node<int>* history;
 		long correct;
 		long total;
 	public:
-		DoubleBimodal(){this->correct=this->total=0;};
+		DoubleBimodal(){this->correct=this->total=0;this->history=NULL;};
 		double percent(){return ((double) this->correct)/this->total;};
 		double predict(std::ifstream*);
 };
