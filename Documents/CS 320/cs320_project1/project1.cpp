4039163c8fd41c80c4208bb0a2d7b83f6b85cc60
Michael Quinnemail <mquinn5@binghamton.edu>
2018-03-18 16:15:37 -0400

    corrected reset

--- Documents/CS 320/cs320_project1/project1.cpp
+++ Documents/CS 320/cs320_project1/project1.cpp
@@ -25,19 +25,23 @@ int main(int argc, char *argv[]){
 	cout << "Reading " << name << endl;
 	AlwaysTaken alwaysTaken = AlwaysTaken();
 	cout << "Always Taken: " << alwaysTaken.predict(&file) << endl;
-	file.seekg(0);
+	file.clear();
+	file.seekg(0, ios_base::beg);
 	
 	NeverTaken neverTaken = NeverTaken();
 	cout << "Never Taken: " << neverTaken.predict(&file) << endl;
-	file.seekg(0);
+	file.clear();
+	file.seekg(0, ios_base::beg);
 	
 	SingleBimodal singleBimodal = SingleBimodal();
 	cout << "Single Bimodal: " << singleBimodal.predict(&file) << endl;
-	file.seekg(0);
+	file.clear();
+	file.seekg(0, ios_base::beg);
 	
 	DoubleBimodal doubleBimodal = DoubleBimodal();
 	cout << "Double Bimodal: " << doubleBimodal.predict(&file) << endl;
-	file.seekg(0);
+	file.clear();
+	file.seekg(0, ios_base::beg);
 	
 	
 	
