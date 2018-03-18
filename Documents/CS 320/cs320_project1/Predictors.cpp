51d2258d127ad68636de8c9237590c729ee90014
Michael Quinnemail <mquinn5@binghamton.edu>
2018-03-18 15:54:13 -0400

    resolving minor issues

--- Documents/CS 320/cs320_project1/Predictors.cpp
+++ Documents/CS 320/cs320_project1/Predictors.cpp
@@ -40,7 +40,7 @@ double NeverTaken::predict(ifstream * file){
 double SingleBimodal::predict(ifstream * file){
 	char str[MAX_SIZE];
 	while(readTheLine(file,str)){
-		unsigned long index = stoul((string str).substr(2,8), nullptr, 16);
+		unsigned long index = stoul(string(str).substr(2,8), nullptr, 16);
 		bool num = this->history[index];
 		if(str[11]=='T' && num)
 			this->correct++;
@@ -56,7 +56,7 @@ double SingleBimodal::predict(ifstream * file){
 double DoubleBimodal::predict(ifstream * file){
 	char str[MAX_SIZE];
 	while(readTheLine(file,str)){
-		unsigned long index = stoul((string str).substr(2,8), nullptr, 16);
+		unsigned long index = stoul(string(str).substr(2,8), nullptr, 16);
 		int num = this->history[index];
 		if(str[11]=='T'){
 			if(num>1)
